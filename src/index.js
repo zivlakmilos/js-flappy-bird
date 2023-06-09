1c2cd5adfe978197d73070f3f4a4fdfdfbf27abf
Milos Zivlak <zivlakmilos@gmail.com>
2023-06-09 06:11:27 +0200

    Pipes

--- src/index.js
+++ src/index.js
@@ -8,9 +8,6 @@ const config = {
     default: 'arcade',
     arcade: {
       debug: true,
-      gravity: {
-        y: 400,
-      },
     },
   },
   scene: {
@@ -20,23 +17,37 @@ const config = {
   }
 }
 
-function preload() {
-  this.load.image('sky', 'assets/sky.png');
-  this.load.image('bird', 'assets/bird.png');
-}
+const PIPES_TO_RENDER = 4;
 
 let bird = null;
+
+const pipeVerticalDistanceRange = [150, 250];
+let pipeHorizontalDistance = 0;
+
 const initialBirdPosition = {
   x: config.width / 10,
   y: config.height / 2,
 }
 const flapVelocity = 250;
 
+function preload() {
+  this.load.image('sky', 'assets/sky.png');
+  this.load.image('bird', 'assets/bird.png');
+  this.load.image('pipe', 'assets/pipe.png');
+}
+
 function create() {
   this.add.image(0, 0, 'sky').setOrigin(0, 0);
 
   bird = this.physics.add.sprite(initialBirdPosition.x, initialBirdPosition.y, 'bird').setOrigin(0, 0);
-  //bird.body.gravity.y = 200;
+  bird.body.gravity.y = 400;
+
+  for (let i = 0; i < PIPES_TO_RENDER; i++) {
+    const upperPipe = this.physics.add.sprite(0, 0, 'pipe').setOrigin(0, 1);
+    const lowerPipe = this.physics.add.sprite(0, 0, 'pipe').setOrigin(0, 0);
+
+    placePipe(upperPipe, lowerPipe);
+  }
 
   this.input.on('pointerdown', flap);
 
@@ -47,6 +58,22 @@ function update(time, delta) {
   if (bird.y < -bird.height || bird.y > config.height) {
     restartPlayerPosition();
   }
+}
+
+function placePipe(uPipe, lPipe) {
+  pipeHorizontalDistance += 400;
+
+  let pipeVerticalDistance = Phaser.Math.Between(...pipeVerticalDistanceRange);
+  let pipeVerticalPosition = Phaser.Math.Between(20, config.height - 20 - pipeVerticalDistance);
+
+  uPipe.x = pipeHorizontalDistance;
+  uPipe.y = pipeVerticalPosition;
+
+  lPipe.x = pipeHorizontalDistance;
+  lPipe.y = uPipe.y + pipeVerticalDistance;
+
+  uPipe.body.velocity.x = -200;
+  lPipe.body.velocity.x = -200;
 }
 
 function restartPlayerPosition() {
