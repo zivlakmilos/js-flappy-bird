753081778c562a006b7677c0524877ccf83c1bdb
Milos Zivlak <zivlakmilos@gmail.com>
2023-06-13 05:30:12 +0200

    Score Scene

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import Phaser from "phaser";
 import PreloadScene from "./scenes/PreloadScene";
 import MenuScene from "./scenes/MenuScene";
 import PlayScene from "./scenes/PlayScene";
+import ScoreScene from "./scenes/ScoreScene";
 
 const WIDTH = 800;
 const HEIGHT = 600;
@@ -16,7 +17,7 @@ const SHARED_CONFIG = {
   startPosition: BIRD_POSITION,
 }
 
-const Scenes = [PreloadScene, MenuScene, PlayScene];
+const Scenes = [PreloadScene, MenuScene, ScoreScene, PlayScene];
 
 const initScenes = () => {
   return Scenes.map(Scene => new Scene(SHARED_CONFIG));
---
+++ src/scenes/ScoreScene.js
@@ -0,0 +1,16 @@
+import BaseScene from "./BaseScene";
+
+class ScoreScene extends BaseScene {
+  constructor(config) {
+    super('ScoreScene', { ...config, canGoBack: true });
+  }
+
+  create() {
+    super.create();
+
+    const bestScoreText = localStorage.getItem('bestScore');
+    this.add.text(...this.screenCenter, `Score: ${bestScoreText || 0}`, this.fontOptions).setOrigin(0.5);
+  }
+}
+
+export default ScoreScene;
