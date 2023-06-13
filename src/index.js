711d92ee71eb3e46374f1d3ddb41497ac6190bb8
Milos Zivlak <zivlakmilos@gmail.com>
2023-06-13 05:43:05 +0200

    Pause Scene

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import PreloadScene from "./scenes/PreloadScene";
 import MenuScene from "./scenes/MenuScene";
 import PlayScene from "./scenes/PlayScene";
 import ScoreScene from "./scenes/ScoreScene";
+import PauseScene from "./scenes/PauseScene";
 
 const WIDTH = 800;
 const HEIGHT = 600;
@@ -17,7 +18,7 @@ const SHARED_CONFIG = {
   startPosition: BIRD_POSITION,
 }
 
-const Scenes = [PreloadScene, MenuScene, ScoreScene, PlayScene];
+const Scenes = [PreloadScene, MenuScene, ScoreScene, PlayScene, PauseScene];
 
 const initScenes = () => {
   return Scenes.map(Scene => new Scene(SHARED_CONFIG));
--- src/scenes/PlayScene.js
+++ src/scenes/PlayScene.js
@@ -84,6 +84,7 @@ class PlayScene extends BaseScene {
     pauseButton.on('pointerdown', () => {
       this.physics.pause();
       this.scene.pause();
+      this.scene.launch('PauseScene');
     });
   }
 
